337aa9668ebd68fc78b372a41df888dbbc0cf4b8
채가을 <rkdmf0914@naver.com>
2023-10-23 17:24:14 +0900

    Design: 플레이어 / 화면 넘어가기까지 구현

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Player from './Player';
 
 const App = () => {
   return (
     <View>
       <Text>tunemate</Text>
+      <Player/>
     </View>
   );
 };
