f6eadea9ca6ad4e39efeb647054acb35b3afd179
채가을 <rkdmf0914@naver.com>
2023-10-23 17:52:42 +0900

    Design: 디스크 돌리기 진행 중

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -8,6 +8,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RootStack from './screens/RootStack';
+import Player from './Player'
 
 const queryClient = new QueryClient();
 
@@ -20,11 +21,15 @@ function App(): JSX.Element {
             behavior={Platform.select({ ios: 'padding' })}
             style={avoid}
           >
+          
             <NavigationContainer>
+            <Player/>
               <RootStack />
+             
             </NavigationContainer>
           </KeyboardAvoidingView>
         </SafeAreaView>
+       
       </QueryClientProvider>
     </RecoilRoot>
   );
--- FE/src/Player.js
+++ FE/src/Player.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState, Component } from 'react';
+import { StyleSheet, View, Text, Image, TouchableOpacity, Animated, Easing } from 'react-native';
 
 export default function Player() {
   const bgArray = [
@@ -46,11 +46,44 @@ export default function Player() {
     return pointButtons;
   };
 
+  const diskRotationValue = new Animated.Value(0);
+
+  const startDiskRotation = () => {
+    Animated.loop(
+      Animated.timing(diskRotationValue, {
+        toValue: 1,
+        duration: 10000, // 10 seconds for one full rotation
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start();
+  };
+
+  startDiskRotation();
+
+  const interpolatedDiskRotation = diskRotationValue.interpolate({
+    inputRange: [0, 0],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
     <View style={styles.contentWrap}>
-      <View style={styles.album}>
-        <View style={styles.disk}>
-          <View style={styles.disk_inner}></View>
+      <View
+        style={[
+          styles.album,
+          styles.albumActive,
+          styles.coverImg,
+        ]}
+      >
+        <View style={styles.diskContainer}>
+          <Animated.View
+            style={[
+              styles.disk,
+              { transform: [{ rotate: interpolatedDiskRotation }] },
+            ]}
+          >
+            <View style={styles.diskInner}></View>
+          </Animated.View>
         </View>
         <TouchableOpacity style={styles.coverImg}>
           <Image
@@ -60,9 +93,7 @@ export default function Player() {
         </TouchableOpacity>
         <Text>{pageNum}</Text>
       </View>
-      <View style={styles.pointWrap}>
-        {renderPointButtons()}
-      </View>
+      <View style={styles.pointWrap}>{renderPointButtons()}</View>
       <View style={styles.buttonWrap}>
         <TouchableOpacity style={styles.button} onPress={handlePrev}>
           <Text>PREV</Text>
@@ -77,124 +108,110 @@ export default function Player() {
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   overflow: 'hidden',
-  //   backgroundColor: 'linear-gradient(120deg, #0272a4, #f6a564)',
-  // },
-  // contentWrap: {
-  //   width: '100%',
-  //   height: '100%',
-  // },
-  // album: {
-  //   position: 'absolute',
-  //   top: '47%',
-  //   left: '55%',
-  //   transform: [{ translateX: -50 }, { translateY: -50 }],
-  //   width: 600,
-  //   height: 400,
-  //   opacity: 0,
-  //   transition: 'all .4s ease-in-out',
-  // },
-  // albumMobile: {
-  //   top: '40%',
-  //   width: 300,
-  //   height: 200,
-  // },
-  // albumActive: {
-  //   opacity: 1,
-  //   left: '50%',
-  // },
-  // albumActiveMobile: {
-  //   left: '45%',
-  // },
-  // coverImg: {
-  //   position: 'absolute',
-  //   width: 400,
-  //   height: 400,
-  //   borderRadius: 20,
-  //   shadowColor: 'rgba(0, 0, 0, 0.3)',
-  //   shadowOffset: { width: 2, height: 14 },
-  //   shadowRadius: 40,
-  // },
-  // coverImgMobile: {
-  //   width: 250,
-  //   height: 250,
-  // },
-  // disk: {
-  //   position: 'absolute',
-  //   top: 4,
-  //   left: 0,
-  //   width: 392,
-  //   height: 392,
-  //   borderRadius: 196,
-  //   backgroundColor: 'linear-gradient(120deg, #000, #333333, #000)',
-  //   shadowColor: 'rgba(0, 0, 0, 0.3)',
-  //   shadowOffset: { width: 4, height: 14 },
-  //   shadowRadius: 40,
-  //   transition: 'left 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-  //   animation: 'rotateAni 10s linear infinite',
-  // },
-  // diskMobile: {
-  //   width: 244,
-  //   height: 244,
-  //   borderRadius: 122,
-  // },
-  // diskInner: {
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: [{ translateX: -50 }, { translateY: -50 }],
-  //   width: 160,
-  //   height: 160,
-  //   backgroundColor: '#0473a4',
-  //   borderRadius: 80,
-  //   borderWidth: 3,
-  //   borderColor: 'rgba(255, 255, 255, 0.4)',
-  // },
-  // diskInnerMobile: {
-  //   width: 100,
-  //   height: 100,
-  // },
-  // diskInnerDot: {
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: [{ translateX: -50 }, { translateY: -50 }],
-  //   width: 7,
-  //   height: 7,
-  //   borderRadius: 3.5,
-  //   backgroundColor: 'black',
-  // },
-  // buttonWrap: {
-  //   position: 'absolute',
-  //   bottom: '5%',
-  //   width: '100%',
-  //   flexDirection: 'row',
-  //   justifyContent: 'center',
-  // },
-  // button: {
-  //   padding: 6,
-  //   margin: 3,
-  //   backgroundColor: '#000',
-  //   color: '#fff',
-  // },
-  // buttonHover: {
-  //   backgroundColor: '#fff',
-  //   color: '#000',
-  // },
-  // pointWrap: {
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  // },
-  // point: {
-  //   width: 10,
-  //   height: 10,
-  //   margin: 5,
-  //   borderRadius: 5,
-  //   backgroundColor: 'rgba(0, 0, 0, 0.4)',
-  // },
-  // pointActive: {
-  //   backgroundColor: 'black',
-  // },
+  contentWrap: {
+    width: '100%',
+    height: '100%',
+  },
+  album: {
+    position: 'absolute',
+    transform: [{ translateX: -150 }, { translateY: 150 }],
+    width: 200,
+    height: 200,
+    opacity: 0,
+    transition: 'all .4s ease-in-out',
+  },
+  albumActive: {
+    opacity: 1,
+    left: '50%',
+  },
+  coverImg: {
+    position: 'absolute',
+    width: 200,
+    height: 200,
+    borderRadius: 20,
+    shadowColor: 'rgba(0, 0, 0, 0.3)',
+    shadowOffset: { width: 2, height: 14 },
+    shadowRadius: 40,
+  },
+  diskContainer: {
+    position: 'absolute',
+    top: 4,
+    left: 0,
+    width: 392,
+    height: 392,
+  },
+  disk: {
+    position: 'absolute',
+    top: 4,
+    left: 0,
+    width: 392,
+    height: 392,
+    borderRadius: 196,
+    backgroundColor: 'transparent',
+    shadowColor: 'rgba(0, 0, 0, 0.3)',
+    shadowOffset: { width: 4, height: 14 },
+    shadowRadius: 40,
+  },
+  diskMobile: {
+    width: 244,
+    height: 244,
+    borderRadius: 122,
+  },
+  diskInner: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: 50 }, { translateY: 50 }],
+    width: 160,
+    height: 160,
+    backgroundColor: '#0473a4',
+    borderRadius: 80,
+    borderWidth: 3,
+    borderColor: 'rgba(255, 255, 255, 0.4)',
+  },
+  diskInnerMobile: {
+    width: 100,
+    height: 100,
+  },
+  diskInnerDot: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: 50 }, { translateY: 50 }],
+    width: 7,
+    height: 7,
+    borderRadius: 3.5,
+    backgroundColor: 'black',
+  },
+  buttonWrap: {
+    position: 'absolute',
+    bottom: '5%',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  button: {
+    padding: 6,
+    margin: 3,
+    backgroundColor: '#000',
+    color: '#fff',
+  },
+  buttonHover: {
+    backgroundColor: '#fff',
+    color: '#000',
+  },
+  pointWrap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  point: {
+    width: 10,
+    height: 10,
+    margin: 5,
+    borderRadius: 5,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  pointActive: {
+    backgroundColor: 'black',
+  },
 });
