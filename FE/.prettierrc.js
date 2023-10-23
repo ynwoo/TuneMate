c790ea17ca2b8b2658396fa4d63e61a498961f69
노태균 <taegyunn123@gmail.com>
2023-10-23 21:51:28 +0900

    chore: eslint & prettier 설정 수정

--- FE/.prettierrc.js
+++ FE/.prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  arrowParens: 'all',
+  arrowParens: 'always',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: "auto",
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
 };
