5769633e5a2ea35e1f158640600c1dda4cb027b1
고범수 <rhqjatn2398@naver.com>
2023-10-24 17:35:59 +0900

    Feat: API Gateway 프로젝트 생성

---
+++ tunemate-api-gateway/src/test/java/com/example/tunemateapigateway/TunemateApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tunemateapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunemateApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
