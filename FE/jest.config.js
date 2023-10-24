07ec9d1642774bb0094c4b2a61ad9631a0e90e87
taegn <taegyunn123@gmail.com>
2023-10-24 09:25:42 +0900

    chore: babel 설정 수정

--- FE/jest.config.js
+++ FE/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation|recoil)'
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation|recoil)',
   ],
   moduleNameMapper: {
-    recoil: 'recoil/native'
-  }
+    recoil: 'recoil/native',
+  },
 };
