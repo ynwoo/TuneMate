2abf099110486fdf56413d4085ada8a5c2b1c06e
xjffla12 <xjffla12@naver.com>
2023-10-26 14:23:16 +0900

    Feat: 공동 플레이리스트에 노래 추가

--- tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/service/IndividualPlaylistService.java
+++ tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/service/IndividualPlaylistService.java
@@ -1,21 +1,20 @@
 package com.tunemate.tunemateplaylist.service;
 
 import com.tunemate.tunemateplaylist.domain.Playlist;
-import com.tunemate.tunemateplaylist.dto.PlaylistCreateDto;
-import com.tunemate.tunemateplaylist.dto.PlaylistIdDto;
-import com.tunemate.tunemateplaylist.dto.PlaylistResponseDto;
-import com.tunemate.tunemateplaylist.dto.TrackCreateDto;
+import com.tunemate.tunemateplaylist.dto.*;
 import org.json.simple.parser.ParseException;
 
 public interface IndividualPlaylistService {
 
     void createPlaylist(long userId, PlaylistCreateDto playlistCreateDto) throws ParseException;
 
-    void createTrack(long userId, TrackCreateDto trackCreateDto);
+    void createTrack(long userId, TrackCreateDto trackCreateDto, String playlistId);
 
-    PlaylistResponseDto getIndividualPlaylist(long userId) throws ParseException;
+    PlaylistResponseDto getIndividualPlaylist(long userId,String playlistId) throws ParseException;
 
     void setIndividualPlaylistId(long userId, PlaylistIdDto playlistIdDto);
 
     void counting(long userId) throws ParseException;
+
+    void deleteTrack(String playlistId, TrackDeleteRequestDto trackDeleteRequestDto);
 }
