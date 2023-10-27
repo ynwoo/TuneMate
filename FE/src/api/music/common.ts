a3e794ef771e173b7c2ceb6f9b09755e3a62308b
taegn <taegyunn123@gmail.com>
2023-10-27 14:36:41 +0900

    feat: music - individual & common 메서드 명 정의

---
+++ FE/src/api/music/common.ts
@@ -0,0 +1,18 @@
+// 공동 플레이리스트 조회
+const getCommonPlayList = () => {};
+// 공동 플레이리스트 생성
+const createCommonPlayList = () => {};
+// 공동 플레이리스트 트랙 추가
+const createCommonPlayListTrack = () => {};
+// 공동 플레이리스트 트랙 삭제
+const deleteCommonPlayListTrack = () => {};
+// 공동 플레이리스트 트랙 순서 변경
+const updateCommonPlayListTrack = () => {};
+
+export {
+  getCommonPlayList,
+  createCommonPlayList,
+  createCommonPlayListTrack,
+  deleteCommonPlayListTrack,
+  updateCommonPlayListTrack,
+};
---
+++ FE/src/api/music/individual.ts
@@ -0,0 +1,27 @@
+// 개인 플레이리스트 생성
+const createIndividualPlayList = () => {};
+// 개인 대표 플레이리스트 조회
+const getIndividualPlayListRepresentative = () => {};
+// 개인 플레이리스트 목록조회
+const getIndividualPlayLists = () => {};
+// 개인 플레이리스트 트랙 추가
+const createIndividualPlayListTrack = () => {};
+// 개인 플레이리스트 트랙 삭제
+const deleteIndividualPlayListTrack = () => {};
+// 개인 플레이리스트 트랙 순서 변경
+const updateIndividualPlayListTrack = () => {};
+// 대표 플레이리스트 설정
+const updateIndividualPlayList = () => {};
+// 노래 재생 카운트
+const addIndividualMusicCount = () => {};
+
+export {
+  createIndividualPlayList,
+  getIndividualPlayListRepresentative,
+  getIndividualPlayLists,
+  createIndividualPlayListTrack,
+  deleteIndividualPlayListTrack,
+  updateIndividualPlayListTrack,
+  updateIndividualPlayList,
+  addIndividualMusicCount,
+};
