34c159865457e1e566a69c63b9387abdd61bb9e6
고범수 <rhqjatn2398@naver.com>
2023-10-27 17:32:45 +0900

    feat: 유저 서비스 프로젝트 생성

---
+++ tunemate-user-service/src/test/java/com/example/tunemateuserservice/TunemateUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tunemateuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunemateUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
