237236c58a61f8c8d5fd9700ecf60abfce87a431
Bumsoo <rhqjatn2398@naver.com>
2023-10-29 16:35:19 +0900

    feat: Member 엔티티 컬럼 제약조건 설정

--- tunemate-user-service/src/main/java/com/example/tunemateuserservice/model/Member.java
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/model/Member.java
@@ -1,6 +1,7 @@
 package com.example.tunemateuserservice.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,20 +19,27 @@ public class Member {
     @Schema(description = "기본키")
     private Long id;
     @Schema(description = "API용 사용자 ID")
+    @Column(nullable = false, unique = true)
     private String userId;
     @Schema(description = "사용자 이름")
+    @Column(nullable = false)
     private String name;
     @Schema(description = "이메일")
+    @Column(nullable = false, unique = true)
     private String email;
     @Schema(description = "이미지 URL")
     private String imageUrl;
     @Schema(description = "스포티파이 유저 아이디")
+    @Column(nullable = false, unique = true)
     private String spotifyUserId;
     @Schema(description = "Tunemate 리프레시 토큰")
+    @Column(nullable = false)
     private String refreshToken;
     @Schema(description = "스포티파이 액세스 토큰")
+    @Column(nullable = false)
     private String spotifyAccessToken;
     @Schema(description = "스포티파이 리프레시 토큰")
+    @Column(nullable = false)
     private String spotifyRefreshToken;
 
     @Builder
