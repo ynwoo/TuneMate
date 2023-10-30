a03705b54cbec60eac04859270d60f43f5982fdd
고범수 <rhqjatn2398@naver.com>
2023-10-30 13:49:00 +0900

    feat: AUthenticationSuccessHandler 등록
    - OAuth 인증 성공시 로직이 실행되도록 등록함

--- tunemate-user-service/src/main/java/com/example/tunemateuserservice/config/WebConfig.java
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/config/WebConfig.java
@@ -6,13 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, MemberService memberService) throws Exception {
-        http.oauth2Login(oauth2login-> oauth2login.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(memberService)));
+    public SecurityFilterChain filterChain(HttpSecurity http, MemberService memberService, AuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
+        http.oauth2Login(oauth2login -> oauth2login.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(memberService))
+                .successHandler(authenticationSuccessHandler));
 
         return http.build();
     }
