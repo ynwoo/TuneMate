8ec53413632b4d3fdc6692c486c082d97e43a5c9
고범수 <rhqjatn2398@naver.com>
2023-10-30 13:42:12 +0900

    feat: ModelMapper 사용을 위해 Setter 설정
    - MapStruct로 변경하면 Setter가 필요없다

--- tunemate-user-service/src/main/java/com/example/tunemateuserservice/model/Member.java
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/model/Member.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Setter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
