609610f070cd581d022fdf7e5e7c3654caf8d244
고범수 <rhqjatn2398@naver.com>
2023-10-30 13:42:57 +0900

    feat: 기본 생성자 추가 및 Column 어노테이션 제거

--- tunemate-user-service/src/main/java/com/example/tunemateuserservice/dto/MemberDto.java
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/dto/MemberDto.java
@@ -1,36 +1,41 @@
 package com.example.tunemateuserservice.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
+@NoArgsConstructor
 public class MemberDto {
     @Schema(description = "API용 사용자 ID")
-    @Column(nullable = false, unique = true)
     private String userId;
     @Schema(description = "스포티파이 display_name")
-    @Column(nullable = false)
     private String name;
     @Schema(description = "이메일")
-    @Column(nullable = false, unique = true)
     private String email;
     @Schema(description = "이미지 URL")
     private String imageUrl;
     @Schema(description = "스포티파이 유저 아이디")
-    @Column(nullable = false, unique = true)
     private String spotifyUserId;
     @Schema(description = "Tunemate 리프레시 토큰")
-    @Column(nullable = false)
     private String refreshToken;
     @Schema(description = "스포티파이 액세스 토큰")
-    @Column(nullable = false)
     private String spotifyAccessToken;
     @Schema(description = "스포티파이 리프레시 토큰")
-    @Column(nullable = false)
     private String spotifyRefreshToken;
+
+    @Builder
+    public MemberDto(String userId, String name, String email, String imageUrl, String spotifyUserId, String refreshToken, String spotifyAccessToken, String spotifyRefreshToken) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.spotifyUserId = spotifyUserId;
+        this.refreshToken = refreshToken;
+        this.spotifyAccessToken = spotifyAccessToken;
+        this.spotifyRefreshToken = spotifyRefreshToken;
+    }
 }
