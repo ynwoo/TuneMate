d494345b76abfee16c77abf6afb243cdaa59e4cb
고범수 <rhqjatn2398@naver.com>
2023-10-31 16:54:49 +0900

    feat: FeignClient 요청에 UserId 헤더 추가

--- tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/client/UserServiceClient.java
+++ tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/client/UserServiceClient.java
@@ -1,13 +1,16 @@
 package com.tunemate.tunemateplaylist.client;
 
 import com.tunemate.tunemateplaylist.vo.MemberInfo;
+import feign.HeaderMap;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
 
     @GetMapping("/users/{userId}")
-    MemberInfo getMember(@PathVariable String userId);
+    MemberInfo getMember(@HeaderMap Map<String, Object> headers, @RequestParam String userId);
 }
