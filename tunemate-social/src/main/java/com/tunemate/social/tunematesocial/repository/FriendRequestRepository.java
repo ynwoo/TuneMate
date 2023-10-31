0f6e5685c57fcb68c50e53ff336f47cd2b9a0d31
ynwoo <ynwookim98@gmail.com>
2023-10-31 12:58:14 +0900

    Feat: JPA 엔티티 및 레포지토리 설정

---
+++ tunemate-social/src/main/java/com/tunemate/social/tunematesocial/repository/FriendRequestRepository.java
@@ -0,0 +1,9 @@
+package com.tunemate.social.tunematesocial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tunemate.social.tunematesocial.entity.FriendRequest;
+
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+
+}
