32faabedd88f156d1535c2aa0c341f5c720cb429
채가을 <rkdmf0914@naver.com>
2023-10-31 17:50:41 +0900

    Docs: Autumn's 2023-10-31 TIL

--- FE/src/screens/PlayerScreen.tsx
+++ FE/src/screens/PlayerScreen.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import {
+  ScrollView,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 import Player from '@/components/player/Player';
+import Playlist from '@/components/playlist/Playlist';
+import Sound from '@/components/player/sound';
 
 const PlayerScreen = () => {
-  return <Player />;
+  return (
+    <GestureHandlerRootView>
+      <ScrollView>
+        <Sound />
+        <View>
+          <Player />
+
+          <View style={styles.playList}>
+            <Playlist />
+          </View>
+        </View>
+      </ScrollView>
+    </GestureHandlerRootView>
+  );
 };
 
 export default PlayerScreen;
+
+const styles = StyleSheet.create({
+  playList: {
+    bottom: 120,
+  },
+});
