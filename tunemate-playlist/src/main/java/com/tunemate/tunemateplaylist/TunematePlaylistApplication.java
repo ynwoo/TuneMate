20209f7cb218a342e97f8120bbdb02b57eea71a4
고범수 <rhqjatn2398@naver.com>
2023-10-31 17:23:20 +0900

    fix: FeignLogger 레벨 등록

--- tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/TunematePlaylistApplication.java
+++ tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/TunematePlaylistApplication.java
@@ -1,9 +1,11 @@
 package com.tunemate.tunemateplaylist;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,4 +16,8 @@ public class TunematePlaylistApplication {
         SpringApplication.run(TunematePlaylistApplication.class, args);
     }
 
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
