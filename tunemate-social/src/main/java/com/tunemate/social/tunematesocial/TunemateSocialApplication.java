77f4d7d072d06439d7f189cf332fd6ef77ac375f
ynwoo <ynwookim98@gmail.com>
2023-10-31 11:03:21 +0900

    Init: Social Spring Project

---
+++ tunemate-social/src/main/java/com/tunemate/social/tunematesocial/TunemateSocialApplication.java
@@ -0,0 +1,13 @@
+package com.tunemate.social.tunematesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunemateSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunemateSocialApplication.class, args);
+	}
+
+}
