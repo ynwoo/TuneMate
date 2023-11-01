745861067ff99783be34d5df649ab60ad8472591
고범수 <rhqjatn2398@naver.com>
2023-11-01 13:13:53 +0900

    feat: 사용자 ID 목록으로 사용자 정보 목록 조회 구현

--- tunemate-user-service/src/main/java/com/example/tunemateuserservice/controller/MemberController.java
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/controller/MemberController.java
@@ -9,10 +9,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,4 +32,8 @@ public class MemberController {
         return ResponseEntity.ok(responseMember);
     }
 
+    @PostMapping
+    public ResponseEntity<List<ResponseMember>> getMembersByUserIdIn(@RequestBody List<String> userIds) {
+        return ResponseEntity.ok(memberService.getMembersByUserIdIn(userIds).stream().map(memberDto -> mapper.map(memberDto, ResponseMember.class)).toList());
+    }
 }
