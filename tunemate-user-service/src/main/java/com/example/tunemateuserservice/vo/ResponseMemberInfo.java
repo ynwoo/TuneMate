7d39c2e059a5caaa6b16960834b7edfbd27cef67
고범수 <rhqjatn2398@naver.com>
2023-11-01 14:59:18 +0900

    feat: ResponseMemberInfo 추가

---
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/vo/ResponseMemberInfo.java
@@ -0,0 +1,10 @@
+package com.example.tunemateuserservice.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseMemberInfo {
+    private String userId;
+    private String name;
+    private String imageUrl;
+}
