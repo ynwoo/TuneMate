b1a7ad9258d67c4cec7093f96394be2ebfa931b0
고범수 <rhqjatn2398@naver.com>
2023-11-01 10:52:06 +0900

    fix: UserServiceClient Header 추가 되도록 수정

--- tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/client/UserServiceClient.java
+++ tunemate-playlist/src/main/java/com/tunemate/tunemateplaylist/client/UserServiceClient.java
@@ -1,11 +1,10 @@
 package com.tunemate.tunemateplaylist.client;
 
 import com.tunemate.tunemateplaylist.vo.MemberInfo;
-import feign.HeaderMap;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.Map;
 
@@ -13,5 +12,5 @@ import java.util.Map;
 public interface UserServiceClient {
 
     @GetMapping("/users/{userId}")
-    MemberInfo getMember(@HeaderMap Map<String, Object> headers, @PathVariable String userId);
+    MemberInfo getMember(@RequestHeader Map<String, String> headers, @PathVariable String userId);
 }
