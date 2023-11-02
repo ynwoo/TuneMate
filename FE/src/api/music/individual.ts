fbd1c67f55f547144a47f5b943245b74ca39001e
taegn <taegyunn123@gmail.com>
2023-11-02 13:46:40 +0900

    feat: axios default baseUrl & token 설정

--- FE/src/api/music/individual.ts
+++ FE/src/api/music/individual.ts
@@ -1,19 +1,19 @@
 import { NewPlayList, PlayList, TotalPlayList } from '@/types/playList';
-import { authApi } from '..';
 import { AddTrack, ChangeTrack, DeleteTrack } from '@/types/spotify';
+import axios from 'axios';
 
 const INDIVIDUAL_PLAYLISTS_URL = 'music/individual/playlists';
 
 // 개인 플레이리스트 생성
 const createIndividualPlayList = async (newPlayList: NewPlayList) => {
-  await authApi.post<void>(INDIVIDUAL_PLAYLISTS_URL, newPlayList);
+  await axios.post<void>(INDIVIDUAL_PLAYLISTS_URL, newPlayList);
 };
 
 // 개인 대표 플레이리스트 조회
 const getIndividualPlayListRepresentative = async (
   playlistId: PlayList['id'],
 ): Promise<PlayList> => {
-  const response = await authApi.get<PlayList>(
+  const response = await axios.get<PlayList>(
     `${INDIVIDUAL_PLAYLISTS_URL}-representative/${playlistId}`,
   );
   return response.data;
@@ -21,7 +21,7 @@ const getIndividualPlayListRepresentative = async (
 
 // 개인 플레이리스트 목록조회
 const getIndividualPlayLists = async (): Promise<TotalPlayList> => {
-  const response = await authApi.get<TotalPlayList>(INDIVIDUAL_PLAYLISTS_URL);
+  const response = await axios.get<TotalPlayList>(INDIVIDUAL_PLAYLISTS_URL);
   return response.data;
 };
 
@@ -31,7 +31,7 @@ const createIndividualPlayListTrack = async ({
   uris,
   position,
 }: AddTrack) => {
-  await authApi.post<void>(`${INDIVIDUAL_PLAYLISTS_URL}/${playlistId}/tracks`, {
+  await axios.post<void>(`${INDIVIDUAL_PLAYLISTS_URL}/${playlistId}/tracks`, {
     uris,
     position,
   });
@@ -43,10 +43,9 @@ const deleteIndividualPlayListTrack = async ({
   uri,
   positions,
 }: DeleteTrack) => {
-  await authApi.delete<void>(
-    `${INDIVIDUAL_PLAYLISTS_URL}/${playlistId}/tracks`,
-    { tracks: [{ uri, positions }] },
-  );
+  await axios.delete<void>(`${INDIVIDUAL_PLAYLISTS_URL}/${playlistId}/tracks`, {
+    tracks: [{ uri, positions }],
+  });
 };
 
 // 개인 플레이리스트 트랙 순서 변경
@@ -54,7 +53,7 @@ const updateIndividualPlayListTrack = async ({
   playlistId,
   changeTrackIndex,
 }: ChangeTrack) => {
-  await authApi.put<void>(
+  await axios.put<void>(
     `${INDIVIDUAL_PLAYLISTS_URL}/${playlistId}/tracks`,
     changeTrackIndex,
   );
@@ -62,12 +61,12 @@ const updateIndividualPlayListTrack = async ({
 
 // 대표 플레이리스트 설정
 const updateIndividualPlayList = async (playlistId: PlayList['id']) => {
-  await authApi.put<void>(INDIVIDUAL_PLAYLISTS_URL, { playlistId });
+  await axios.put<void>(INDIVIDUAL_PLAYLISTS_URL, { playlistId });
 };
 
 // 노래 재생 카운트
 const addIndividualMusicCount = async () => {
-  await authApi.post<void>(`music/individual/count`);
+  await axios.post<void>(`music/individual/count`);
 };
 
 export {
--- FE/src/api/recommendation.ts
+++ FE/src/api/recommendation.ts
@@ -1,9 +1,9 @@
 import { RecommendationFriend } from '@/types/social';
-import { authApi } from '.';
+import axios from 'axios';
 
 // 추천 친구 목록 조회
 export const getRecommendationFriends = async () => {
-  const response = await authApi.get<RecommendationFriend[]>(
+  const response = await axios.get<RecommendationFriend[]>(
     `recommendation/friends`,
   );
   return response.data;
