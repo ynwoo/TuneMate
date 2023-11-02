846091bae12510b3c57e2b649b90524abecac50d
xjffla12 <xjffla12@naver.com>
2023-11-02 14:27:13 +0900

    Feat: 추천 친구 반환 데이터 수정

--- tunemate-recommend-service/main.py
+++ tunemate-recommend-service/main.py
@@ -139,7 +139,7 @@ def song(UserId : str | None = Header(default=None)):
 
 # 사람 추천
 @app.get("/api/v1/recommendation/friends", response_model=List[ReturnDto])
-def root(UserId : str | None = Header(default=None)):
+async def root(UserId : str | None = Header(default=None)):
     conn = pymysql.connect(user=os.environ["DATABASE_USERNAME"],
                            password=os.environ["DATABASE_PASSWORD"], host=os.environ["DATABASE_URL"],
                            db="MUSIC", port=int(os.environ["DATABASE_PORT"]), charset="utf8")
@@ -196,9 +196,9 @@ def root(UserId : str | None = Header(default=None)):
         sql = "select playlist_spotify_id from playlist where user_id = %s"
         cursor.execute(sql,user)
         playlistId = cursor.fetchone
-        userOb = request(user)
+        userOb = await request(user)
         print(userOb)
         print(userOb.get("userId"))
         responseList.append(ReturnDto(userId=userOb.get("userId"),img=userOb.get("imageUrl"),name=userOb.get("name"),playlistId=playlistId))
     
-    return user_id_responses
+    return responseList
