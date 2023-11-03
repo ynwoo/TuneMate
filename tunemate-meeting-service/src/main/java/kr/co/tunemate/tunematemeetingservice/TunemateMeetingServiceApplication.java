dec7f5aab12c67257102b1617c5da5e9774120f1
xjffla12 <xjffla12@naver.com>
2023-11-03 12:48:49 +0900

    Create: Meeting-Service 프로젝트 생성

---
+++ tunemate-meeting-service/src/main/java/kr/co/tunemate/tunematemeetingservice/TunemateMeetingServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tunemate.tunematemeetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunemateMeetingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunemateMeetingServiceApplication.class, args);
+    }
+
+}
---
+++ tunemate-meeting-service/src/test/java/kr/co/tunemate/tunematemeetingservice/TunemateMeetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tunemate.tunematemeetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunemateMeetingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
