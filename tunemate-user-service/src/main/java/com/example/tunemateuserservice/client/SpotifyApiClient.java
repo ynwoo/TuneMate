b4105a1991ddd01efeb02414d8a81ab3735542b5
고범수 <rhqjatn2398@naver.com>
2023-11-03 09:10:01 +0900

    feat: Spotify API 요청 메서드 작성

---
+++ tunemate-user-service/src/main/java/com/example/tunemateuserservice/client/SpotifyApiClient.java
@@ -0,0 +1,17 @@
+package com.example.tunemateuserservice.client;
+
+import com.example.tunemateuserservice.vo.SpotifyReissueInfo;
+import com.example.tunemateuserservice.vo.SpotifyReissueRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Map;
+
+@FeignClient(name = "spotify-api", url = "https://accounts.spotify.com")
+public interface SpotifyApiClient {
+    @PostMapping(path = "/api/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public SpotifyReissueInfo reissueSpotifyTokens(@RequestHeader Map<String, String> headers, @RequestBody SpotifyReissueRequest spotifyReissueRequest);
+}
