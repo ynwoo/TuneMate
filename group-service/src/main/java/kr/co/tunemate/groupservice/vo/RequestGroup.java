6fc63e23eee3fc1e02d3ad7ecc88a2073debeec4
고범수 <rhqjatn2398@naver.com>
2023-11-06 17:22:36 +0900

    feat: RequestGroup VO groupId 필드 추가

--- group-service/src/main/java/kr/co/tunemate/groupservice/vo/RequestGroup.java
+++ group-service/src/main/java/kr/co/tunemate/groupservice/vo/RequestGroup.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class RequestGroup {
+    private String groupId;
     private String title;
     private Integer capacity;
     private String concertId;
