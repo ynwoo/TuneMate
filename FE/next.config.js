c19f7652c7a0432fe7aae5348a4c6d10ba0e52d2
민동후 <mdh9701@naver.com>
2023-11-06 12:50:13 +0900

    Feat: 예시용 이미지 링크 추가

--- FE/next.config.js
+++ FE/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["3.bp.blogspot.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
