78d6cafe2279435b8a79350b3e9127d9379bfa5b
taegn <taegyunn123@gmail.com>
2023-11-06 11:01:01 +0900

    feat: recommendationItem 구현

--- FE/src/components/recommendation/RecommendationItem/RecommendationItem.tsx
+++ FE/src/components/recommendation/RecommendationItem/RecommendationItem.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Props from "@/types";
 import { RecommendationFriend } from "@/types/social";
+import { classNameWrapper } from "@/utils/className";
+import styles from "./RecommendationItem.module.css";
 
 interface RecommendItemProps extends Props {
   item: RecommendationFriend;
 }
 
-const RecommendationItem = ({ item }: RecommendItemProps) => {
-  return <div>{item.name}</div>;
+const RecommendationItem = ({ item, className }: RecommendItemProps) => {
+  return (
+    <div className={classNameWrapper(className, styles["recommendation-item"])}>
+      <div className={styles["recommendation-item__user"]}>
+        <p className={styles["recommendation-item__user--image"]}>프사</p>
+        <p className={styles["recommendation-item__user--name"]}>{item.name}</p>
+      </div>
+      <div className={styles["recommendation-item__ratio"]}>
+        <p>거리</p>
+        <p>유사도</p>
+      </div>
+    </div>
+  );
 };
 
 export default RecommendationItem;
