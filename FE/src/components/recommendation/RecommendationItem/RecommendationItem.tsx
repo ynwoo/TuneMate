033493fa903c6a7bbbd42eb2bc805bdbc98da0ef
taegn <taegyunn123@gmail.com>
2023-11-06 12:39:19 +0900

    feat: profileImage 구현

--- FE/src/components/recommendation/RecommendationItem/RecommendationItem.tsx
+++ FE/src/components/recommendation/RecommendationItem/RecommendationItem.tsx
@@ -3,6 +3,7 @@ import Props from "@/types";
 import { RecommendationFriend } from "@/types/social";
 import { classNameWrapper } from "@/utils/className";
 import styles from "./RecommendationItem.module.css";
+import ProfileImage from "@/components/image/ProfileImage/ProfileImage";
 
 interface RecommendItemProps extends Props {
   item: RecommendationFriend;
@@ -12,7 +13,12 @@ const RecommendationItem = ({ item, className }: RecommendItemProps) => {
   return (
     <div className={classNameWrapper(className, styles["recommendation-item"])}>
       <div className={styles["recommendation-item__user"]}>
-        <p className={styles["recommendation-item__user--image"]}>프사</p>
+        <ProfileImage
+          className={styles["recommendation-item__user--image"]}
+          src="https://i.scdn.co/image/ab67757000003b824e172b7776591b79a63fcea9"
+          alt="내 프로필"
+          type="recommendation"
+        />
         <p className={styles["recommendation-item__user--name"]}>{item.name}</p>
       </div>
       <div className={styles["recommendation-item__ratio"]}>
