7b71920979a0757d21ffdd011a20a158894c6826
고범수 <rhqjatn2398@naver.com>
2023-11-06 16:57:47 +0900

    feat: Group 생성 VO 작성

---
+++ group-service/src/main/java/kr/co/tunemate/groupservice/vo/RequestGroup.java
@@ -0,0 +1,14 @@
+package kr.co.tunemate.groupservice.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RequestGroup {
+    private String title;
+    private Integer capacity;
+    private String concertId;
+    private LocalDateTime deadline;
+    private String content;
+}
