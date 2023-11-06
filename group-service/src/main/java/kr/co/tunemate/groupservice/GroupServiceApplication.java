519eb3626ceeb120ca4cd3c40ad82cfe7b034bfe
고범수 <rhqjatn2398@naver.com>
2023-11-06 17:23:41 +0900

    feat: ModelMapper 빈 등록

--- group-service/src/main/java/kr/co/tunemate/groupservice/GroupServiceApplication.java
+++ group-service/src/main/java/kr/co/tunemate/groupservice/GroupServiceApplication.java
@@ -1,7 +1,10 @@
 package kr.co.tunemate.groupservice;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GroupServiceApplication {
@@ -10,4 +13,11 @@ public class GroupServiceApplication {
         SpringApplication.run(GroupServiceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return mapper;
+    }
 }
