82b9a7ef8a7866d830272f1163af0eba7fc3ceec
고범수 <rhqjatn2398@naver.com>
2023-11-07 15:24:39 +0900

    feat: Group 엔티티가 BaseTimeEntity 상속하도록 수정
    - 엔티티에 모집 공고 시작일시 속성 추가

--- group-service/src/main/java/kr/co/tunemate/tunemategroupservice/entity/Group.java
+++ group-service/src/main/java/kr/co/tunemate/tunemategroupservice/entity/Group.java
@@ -1,15 +1,19 @@
 package kr.co.tunemate.tunemategroupservice.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "group_table")
-public class Group {
+public class Group extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,6 +25,8 @@ public class Group {
     private Integer capacity;
     @Column(nullable = false)
     private String concertId;
+    @CreatedDate
+    private LocalDateTime startDateTime;
     @Column(nullable = false)
     private LocalDateTime deadline;
     @Column(length = 1024)
