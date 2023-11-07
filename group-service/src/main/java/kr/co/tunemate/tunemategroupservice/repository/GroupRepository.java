82a5440f1a3bddd253129b89aff5ac642dcb9b50
고범수 <rhqjatn2398@naver.com>
2023-11-07 10:14:23 +0900

    feat: 공고ID로 공고를 찾는 추상메서드 작성

--- group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupRepository.java
+++ group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupRepository.java
@@ -3,5 +3,8 @@ package kr.co.tunemate.tunemategroupservice.repository;
 import kr.co.tunemate.tunemategroupservice.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    Optional<Group> findByGroupId(String groupId);
 }
