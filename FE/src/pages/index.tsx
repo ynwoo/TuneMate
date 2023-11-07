f25757e89bb7edb4a207de56be59f8fc7ba850d4
ynwoo <ynwookim98@gmail.com>
2023-11-07 11:04:15 +0900

    Update: 튠메이트 url

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
     <div className={styles["login-page"]}>
       <p className={styles["login-page__title"]}>Tunemate</p>
       <br />
-      <a href="http://k9a603.p.ssafy.io:8000/api/v1/user-service/oauth2/authorization/spotify">
+      <a href="http://tunemate.co.kr/api/v1/user-service/oauth2/authorization/spotify">
         login
       </a>
       <br />
