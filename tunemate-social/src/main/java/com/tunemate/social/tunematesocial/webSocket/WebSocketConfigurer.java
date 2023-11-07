b9195503a08e99f1f1086e33f7ab5f7cb7d4f2d0
xjffla12 <xjffla12@naver.com>
2023-11-07 16:32:25 +0900

    Refactor: RabbitMQ 설정 환경변수로 변경

--- tunemate-social/src/main/java/com/tunemate/social/tunematesocial/webSocket/WebSocketConfigurer.java
+++ tunemate-social/src/main/java/com/tunemate/social/tunematesocial/webSocket/WebSocketConfigurer.java
@@ -1,5 +1,6 @@
 package com.tunemate.social.tunematesocial.webSocket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,17 +11,29 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${HOSTNAME}")
+    private String host;
+
+    @Value("${RABBITMQ_PORT_TWO}")
+    private int port;
+
+    @Value("${RABBITMQ_USERNAME}")
+    private String username;
+
+    @Value("${RABBITMQ_PASSWORD}")
+    private String password;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/pub")
                 .enableStompBrokerRelay("/topic")
-                .setRelayHost("k9A603.p.ssafy.io")
-                .setRelayPort(8007)
+                .setRelayHost(host)
+                .setRelayPort(port)
                 .setVirtualHost("/")
-                .setClientLogin("a603")
-                .setClientPasscode("kimnamgo")
-                .setSystemLogin("a603")
-                .setSystemPasscode("kimnamgo");
+                .setClientLogin(username)
+                .setClientPasscode(password)
+                .setSystemLogin(username)
+                .setSystemPasscode(password);
 
     }
 
