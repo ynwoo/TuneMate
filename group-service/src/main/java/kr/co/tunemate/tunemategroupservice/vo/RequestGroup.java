c34016138c4d8cbba204800e2c0bc74e24cdc561
고범수 <rhqjatn2398@naver.com>
2023-11-07 15:31:37 +0900

    feat: RequestGroup VO description 추가

--- group-service/src/main/java/kr/co/tunemate/tunemategroupservice/vo/RequestGroup.java
+++ group-service/src/main/java/kr/co/tunemate/tunemategroupservice/vo/RequestGroup.java
@@ -1,10 +1,12 @@
 package kr.co.tunemate.tunemategroupservice.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Schema(description = "공고 생성 요청 VO")
 public class RequestGroup {
     private String groupId;
     private String title;
