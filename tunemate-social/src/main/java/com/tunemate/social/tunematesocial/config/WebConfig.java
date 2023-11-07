bc7201b1208ac2a6df0d4cd3c485194397f5f154
고범수 <rhqjatn2398@naver.com>
2023-11-07 13:26:31 +0900

    faet: CORS 설정

---
+++ tunemate-social/src/main/java/com/tunemate/social/tunematesocial/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.tunemate.social.tunematesocial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+}
