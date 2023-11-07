5365f5a4f68ee1e4d49e4306daf53105e1b9bea6
고범수 <rhqjatn2398@naver.com>
2023-11-07 16:39:30 +0900

    test: 공고 조회 서비스 테스트 케이스 작성

--- group-service/src/test/java/kr/co/tunemate/tunemategroupservice/service/GroupServiceImplTest.java
+++ group-service/src/test/java/kr/co/tunemate/tunemategroupservice/service/GroupServiceImplTest.java
@@ -77,4 +77,60 @@ class GroupServiceImplTest {
         // then
         Assertions.assertThatThrownBy(() -> groupService.saveGroup(groupDto));
     }
+
+    @Transactional
+    @DisplayName("공고 UUID로 공고 조회")
+    @Test
+    void getGroupByGroupId() {
+        // given
+        final String name = "엄복동";
+        final String userId = UUID.randomUUID().toString();
+
+        GroupDto groupDto = GroupDto.builder()
+                .groupId(null)
+                .hostId(userId)
+                .hostName(name)
+                .title("공고 제목")
+                .capacity(4)
+                .participantsCnt(1)
+                .concertId("콘서트 UUID")
+                .deadline(LocalDateTime.of(2023, 11, 7, 14, 57))
+                .content("공고 내용")
+                .build();
+
+        groupDto = groupService.saveGroup(groupDto);
+
+        // when
+        GroupDto returnGroupDto = groupService.getGroupByGroupId(groupDto.getGroupId());
+
+        // then
+        Assertions.assertThat(returnGroupDto).isEqualTo(groupDto);
+    }
+
+    @Transactional
+    @DisplayName("존재하지 않는 공고 UUID로 공고 조회")
+    @Test
+    void getGroupByNotExistingGroupId() {
+        // given
+        final String name = "엄복동";
+        final String userId = UUID.randomUUID().toString();
+
+        GroupDto groupDto = GroupDto.builder()
+                .groupId(null)
+                .hostId(userId)
+                .hostName(name)
+                .title("공고 제목")
+                .capacity(4)
+                .participantsCnt(1)
+                .concertId("콘서트 UUID")
+                .deadline(LocalDateTime.of(2023, 11, 7, 14, 57))
+                .content("공고 내용")
+                .build();
+
+        groupDto = groupService.saveGroup(groupDto);
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> groupService.getGroupByGroupId(UUID.randomUUID().toString()));
+    }
 }
