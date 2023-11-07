106b10ee16cd1e66e5ff14b987dd615cd72ff32d
고범수 <rhqjatn2398@naver.com>
2023-11-07 16:20:27 +0900

    test: 테스트 코드 수정

--- group-service/src/test/java/kr/co/tunemate/tunemategroupservice/service/GroupServiceImplTest.java
+++ group-service/src/test/java/kr/co/tunemate/tunemategroupservice/service/GroupServiceImplTest.java
@@ -2,7 +2,6 @@ package kr.co.tunemate.tunemategroupservice.service;
 
 import kr.co.tunemate.tunemategroupservice.dto.GroupDto;
 import kr.co.tunemate.tunemategroupservice.entity.Group;
-import kr.co.tunemate.tunemategroupservice.exception.NoSuchItemException;
 import kr.co.tunemate.tunemategroupservice.repository.GroupRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
 
 @SpringBootTest
 class GroupServiceImplTest {
@@ -30,13 +28,19 @@ class GroupServiceImplTest {
     @Test
     void saveGroup() {
         // given
+        final String name = "엄복동";
+        final String userId = UUID.randomUUID().toString();
+
         GroupDto groupDto = GroupDto.builder()
                 .groupId(null)
+                .hostId(userId)
+                .hostName(name)
                 .title("공고 제목")
-                .content("공고 내용")
-                .deadline(LocalDateTime.of(2023, 11, 7, 14, 57))
-                .concertId("콘서트 UUID")
                 .capacity(4)
+                .participantsCnt(1)
+                .concertId("콘서트 UUID")
+                .deadline(LocalDateTime.of(2023, 11, 7, 14, 57))
+                .content("공고 내용")
                 .build();
 
         // when
@@ -48,18 +52,25 @@ class GroupServiceImplTest {
 
         Assertions.assertThat(returnGroupDto).isEqualTo(mappedDto);
     }
+
     @Transactional
     @DisplayName("공고 제목 없이 공고를 저장")
     @Test
     void saveGroupWithoutTitle() {
         // given
+        final String name = "엄복동";
+        final String userId = UUID.randomUUID().toString();
+
         GroupDto groupDto = GroupDto.builder()
                 .groupId(null)
+                .hostId(userId)
+                .hostName(name)
                 .title(null)
-                .content("공고 내용")
-                .deadline(LocalDateTime.of(2023, 11, 7, 14, 57))
-                .concertId("콘서트 UUID")
                 .capacity(4)
+                .participantsCnt(1)
+                .concertId("콘서트 UUID")
+                .deadline(LocalDateTime.of(2023, 11, 7, 14, 57))
+                .content("공고 내용")
                 .build();
 
         // when
