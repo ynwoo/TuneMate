dbcbbd1cf1c2ebc224541a6071ebcee0638d72ac
xjffla12 <xjffla12@naver.com>
2023-11-07 10:19:07 +0900

    Feat: 내가 속한 채팅방 목록 조회

--- tunemate-social/src/main/java/com/tunemate/social/tunematesocial/service/ChatService.java
+++ tunemate-social/src/main/java/com/tunemate/social/tunematesocial/service/ChatService.java
@@ -1,8 +1,13 @@
 package com.tunemate.social.tunematesocial.service;
 
 import com.tunemate.social.tunematesocial.dto.ChatDto;
+import com.tunemate.social.tunematesocial.dto.response.MyChatRoomListDto;
 import com.tunemate.social.tunematesocial.entity.ChattingRoom;
+
+import java.util.List;
 
 public interface ChatService {
     ChattingRoom getChat(long relationId, ChatDto chatDto);
+
+    List<MyChatRoomListDto> getChatRoomList(String userId);
 }
