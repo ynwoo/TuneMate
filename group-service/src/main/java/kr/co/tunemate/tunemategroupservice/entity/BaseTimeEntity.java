98bddf9e52c8c77286ff586037e7a62d769aaebc
고범수 <rhqjatn2398@naver.com>
2023-11-07 16:15:35 +0900

    feat: createAt 수정 불가 설정

--- group-service/src/main/java/kr/co/tunemate/tunemategroupservice/entity/BaseTimeEntity.java
+++ group-service/src/main/java/kr/co/tunemate/tunemategroupservice/entity/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package kr.co.tunemate.tunemategroupservice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
     @LastModifiedDate
     private LocalDateTime lastModifiedAt;
