c541c5f8c37f37370de99edb06e8f68ef323ca66
고범수 <rhqjatn2398@naver.com>
2023-11-09 14:20:29 +0900

    Feat: 공고 삭제 추상 메서드 작성

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupRepository.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long>, GroupRepositoryCustom {
     Optional<Group> findByGroupId(String groupId);
+    void deleteByHostIdAndGroupId(String groupId, String hostId);
 }
