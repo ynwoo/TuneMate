f4e4deac72c0ad9285b73121a141e08dcd1ba728
고범수 <rhqjatn2398@naver.com>
2023-11-09 12:42:25 +0900

    Refactor: RequestGroup 클래스의 groupId 필드 삭제 및 스웨거 description 작성

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/vo/RequestGroup.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/vo/RequestGroup.java
@@ -6,12 +6,11 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-@Schema(description = "공고 생성 요청 VO")
+@Schema(description = "공고 요청 VO")
 public class RequestGroup {
-    private String groupId;
     private String title;
-    private Integer capacity;
-    private String concertId;
-    private LocalDateTime deadline;
     private String content;
+    private Integer capacity;
+    private LocalDateTime deadline;
+    private String concertId;
 }
