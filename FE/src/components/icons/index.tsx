afb4bea9e9105a1911b72000f26349a0d405945d
민동후 <mdh9701@naver.com>
2023-11-09 13:07:44 +0900

    Feat: 삭제, 핸들 등 아이콘 추가

--- FE/src/components/icons/index.tsx
+++ FE/src/components/icons/index.tsx
@@ -1,5 +1,13 @@
 import { faBell } from "@fortawesome/free-regular-svg-icons/faBell";
-import { faBars, faCirclePlay, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCirclePlay,
+  faUser,
+  faMusic,
+  faTrash,
+  faGripLines,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons/faArrowLeft";
 import { faHouse } from "@fortawesome/free-solid-svg-icons/faHouse";
 import { faMessage } from "@fortawesome/free-solid-svg-icons/faMessage";
@@ -7,7 +15,6 @@ import { faUserPlus } from "@fortawesome/free-solid-svg-icons/faUserPlus";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./icons.module.css";
 import { faComment } from "@fortawesome/free-regular-svg-icons";
-import { faMusic } from "@fortawesome/free-solid-svg-icons/faMusic";
 
 type IconSize = "2xs" | "xs" | "sm" | "lg" | "xl" | "2xl";
 interface IconProps {
@@ -40,10 +47,19 @@ const Icon = {
     <FontAwesomeIcon className={styles.icon} icon={faMusic} size={size} />
   ),
   PlayMusic: ({ size = "2xl" }: IconProps) => (
-    <FontAwesomeIcon className={styles.icon} icon={faCirclePlay} size={size} />
+    <FontAwesomeIcon className={styles.gray} icon={faCirclePlay} size={size} />
   ),
   Menu: ({ size = "2xl" }: IconProps) => (
     <FontAwesomeIcon className={styles.icon} icon={faBars} size={size} />
+  ),
+  Delete: ({ size = "2xl" }: IconProps) => (
+    <FontAwesomeIcon className={styles.gray} icon={faTrash} size={size} />
+  ),
+  Handle: ({ size = "2xl" }: IconProps) => (
+    <FontAwesomeIcon className={styles.gray} icon={faGripLines} size={size} />
+  ),
+  CircleCheck: ({ size = "2xl" }: IconProps) => (
+    <FontAwesomeIcon className={styles.icon} icon={faCircleCheck} size={size} />
   ),
 };
 
