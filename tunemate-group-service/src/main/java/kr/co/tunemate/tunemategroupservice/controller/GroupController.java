a751ed410ece1e932287012bdbabf0ee9a865b17
고범수 <rhqjatn2398@naver.com>
2023-11-09 13:12:25 +0900

    Fix: RequestMapping 어노테이션 사용

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/controller/GroupController.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/controller/GroupController.java
@@ -14,7 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/groups")
+@RestController
+@RequestMapping("/groups")
 @RequiredArgsConstructor
 public class GroupController {
     private final GroupService groupService;
