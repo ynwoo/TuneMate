1493bb6b29442e98329aacc2b2c62effccb64d14
고범수 <rhqjatn2398@naver.com>
2023-11-09 11:05:04 +0900

    Feat: 모집 공고 목록 조회 서비스 메서드 구현

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/service/GroupService.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/service/GroupService.java
@@ -1,9 +1,13 @@
 package kr.co.tunemate.tunemategroupservice.service;
 
 import kr.co.tunemate.tunemategroupservice.dto.GroupDto;
+import kr.co.tunemate.tunemategroupservice.dto.GroupSearchDto;
+
+import java.util.List;
 
 public interface GroupService {
     GroupDto saveGroup(GroupDto groupDto);
     GroupDto getGroupByGroupId(String groupId);
     void closeGroup(String userId, String groupId);
+    List<GroupDto> searchAll(GroupSearchDto groupSearchDto);
 }
