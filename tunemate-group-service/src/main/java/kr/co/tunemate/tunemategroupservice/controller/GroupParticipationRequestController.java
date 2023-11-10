c586c4908f006a2a29afcca593a2630b72462e22
고범수 <rhqjatn2398@naver.com>
2023-11-10 16:54:28 +0900

    Feat: 사용자가 보낸 공고참여요청 목록 조회를 위한 컨트롤러 메서드 작성

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/controller/GroupParticipationRequestController.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/controller/GroupParticipationRequestController.java
@@ -4,15 +4,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import kr.co.tunemate.tunemategroupservice.service.GroupParticipationRequestService;
+import kr.co.tunemate.tunemategroupservice.vo.ResponseGroupParticipationRequest;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class GroupParticipationRequestController {
     private final GroupParticipationRequestService groupParticipationRequestService;
+    private final ModelMapper modelMapper;
 
     @Operation(summary = "공고참여요청을 생성", description = "공고참여요청을 생성합니다.")
     @ApiResponses(
@@ -31,6 +36,21 @@ public class GroupParticipationRequestController {
         groupParticipationRequestService.saveGroupParticipationRequest(userId, groupId);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    
+    @Operation(summary = "보낸 공고참여요청 목록 조회", description = "사용자가 보낸 공고참여요청 목록을 조회합니다.")
+    @ApiResponses(
+            {
+                    @ApiResponse(responseCode = "200", description = "성공")
+            }
+    )
+    @GetMapping("/me/sent-participation-requests")
+    public ResponseEntity<List<ResponseGroupParticipationRequest>> getMyGroupParticipationRequests(@RequestHeader("UserId") String userId) {
+        List<ResponseGroupParticipationRequest> responseGroupParticipationRequests = groupParticipationRequestService.findAllByUserId(userId).stream().map(groupParticipationRequestDto ->
+                modelMapper.map(groupParticipationRequestDto, ResponseGroupParticipationRequest.class)
+        ).toList();
+
+        return ResponseEntity.ok(responseGroupParticipationRequests);
     }
 
     @Operation(summary = "공고에 대한 참여요청을 작성자가 수락", description = "공고에 대한 참여요청을 작성자가 수락합니다.")
