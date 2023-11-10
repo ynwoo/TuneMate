439d56442ad6b1a437ff811c455ff57a80c17f65
고범수 <rhqjatn2398@naver.com>
2023-11-10 09:14:26 +0900

    Feat: 공고참여요청 레포지토리 작성

---
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupParticipationRequestRepository.java
@@ -0,0 +1,6 @@
+package kr.co.tunemate.tunemategroupservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupParticipationRequestRepository extends JpaRepository<GroupParticipationRequestRepository, Long> {
+}
