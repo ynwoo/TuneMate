13069da5f5f401668bf10d5e31284ad9a3aa58b1
xjffla12 <xjffla12@naver.com>
2023-11-10 16:16:46 +0900

    Refactor: 스웨거 추가

--- tunemate-meeting-service/src/main/java/kr/co/tunemate/tunematemeetingservice/controller/MeetingController.java
+++ tunemate-meeting-service/src/main/java/kr/co/tunemate/tunematemeetingservice/controller/MeetingController.java
@@ -1,6 +1,9 @@
 package kr.co.tunemate.tunematemeetingservice.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import kr.co.tunemate.tunematemeetingservice.client.SocialServiceClient;
 import kr.co.tunemate.tunematemeetingservice.domain.Meeting;
 import kr.co.tunemate.tunematemeetingservice.dto.MeetingResponseDto;
@@ -33,9 +36,16 @@ public class MeetingController {
     private final MeetingService meetingService;
 
     @GetMapping("meetings/{relationId}")
-    public ResponseEntity<List<ResponseMeetingList>> getMeetings(@RequestHeader("UserId") String userId, @PathVariable("relationId") long relationId){
-        socialServiceClient.isExistRelation(relationId); // relationId 가 없다면 404 에러 발생
+    @Operation(summary = "만남 목록 조회", description = "선택 한 친구와 설정한 만남 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공."),
+            @ApiResponse(responseCode = "403", description = "권한이 없습니다."),
+            @ApiResponse(responseCode = "404", description = "친구 관계(relationId)가 존재하지 않습니다.")
 
+    })
+    public ResponseEntity<List<ResponseMeetingList>> getMeetings(@RequestHeader("UserId") String userId, @PathVariable("relationId") long relationId){
+        RelationInfo relationInfo = socialServiceClient.isExistRelation(relationId); // relationId 가 없다면 404 에러 발생
+        grantCheck(relationInfo,userId);
         List<ResponseMeetingList> meetingResponseDtoList = meetingService.getMeetings(relationId);
         return ResponseEntity.ok(meetingResponseDtoList);
 
@@ -43,6 +53,13 @@ public class MeetingController {
     }
 
 
+    @Operation(summary = "만남 일정 생성", description = "선택 한 친구와 만남을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "만남 생성 성공."),
+            @ApiResponse(responseCode = "403", description = "권한이 없습니다."),
+            @ApiResponse(responseCode = "404", description = "친구 관계(relationId)가 존재하지 않습니다.")
+
+    })
     @PostMapping("meetings")
     public ResponseEntity createMeeting(@RequestHeader("UserId") String userId, @RequestBody MeetingResponseDto meetingResponseDto){
 
@@ -53,6 +70,13 @@ public class MeetingController {
 
     }
 
+    @Operation(summary = "만남 일정 삭제", description = "선택 한 친구와 설정한 만남을 삭제합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공."),
+            @ApiResponse(responseCode = "403", description = "삭제 권한이 없습니다."),
+            @ApiResponse(responseCode = "404", description = "meetingId가 존재 하지 않습니다.")
+
+    })
     @DeleteMapping("meetings/{meetingId}")
     public ResponseEntity deleteMeeting(@RequestHeader("UserId") String userId, @PathVariable("meetingId") long meetingId){
 
@@ -69,4 +93,11 @@ public class MeetingController {
         meetingService.deleteMeeting(meetingId);
         return ResponseEntity.ok().build();
     }
+
+    public void grantCheck(RelationInfo relationInfoDto, String userId){
+        if(!relationInfoDto.getUser1Id().equals(userId) && !relationInfoDto.getUser2Id().equals(userId)){
+            throw new NotFoundException("권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+    }
+
 }
--- tunemate-meeting-service/src/main/java/kr/co/tunemate/tunematemeetingservice/dto/RelationInfo.java
+++ tunemate-meeting-service/src/main/java/kr/co/tunemate/tunematemeetingservice/dto/RelationInfo.java
@@ -5,7 +5,15 @@ import lombok.Getter;
 @Getter
 public class RelationInfo {
 
+    private String host;
+
     private String user1Id;
 
     private String user2Id;
+
+    private String playlistId;
+
+    private String distance;
+
+    private String similarity;
 }
