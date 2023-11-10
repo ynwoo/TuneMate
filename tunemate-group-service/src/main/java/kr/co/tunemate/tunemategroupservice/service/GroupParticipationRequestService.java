cae1311c8d63fb0dcf689069b9bafcf0aec206a9
고범수 <rhqjatn2398@naver.com>
2023-11-10 14:04:58 +0900

    Feat: 공고참여요청 수락 서비스 메서드 구현

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/service/GroupParticipationRequestService.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/service/GroupParticipationRequestService.java
@@ -1,7 +1,6 @@
 package kr.co.tunemate.tunemategroupservice.service;
 
-import kr.co.tunemate.tunemategroupservice.dto.layertolayer.GroupParticipationRequestDto;
-
 public interface GroupParticipationRequestService {
     void saveGroupParticipationRequest(String userId, String groupId);
+    void acceptGroupParticipationRequest(String userId, String groupParticipationRequestId);
 }
