28ac9b7602eedeb57891b946477bf72f0cce567c
민동후 <mdh9701@naver.com>
2023-11-10 17:30:05 +0900

    메뉴 사용법

---
+++ FE/src/hooks/useMenu.ts
@@ -0,0 +1,36 @@
+import { usePathname } from "next/navigation";
+import { useState, useCallback, useEffect } from "react";
+
+export default function useMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const openToggle = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const closeToggle = useCallback(() => {
+    setIsOpen(false);
+    document.body.style.overflow = "auto";
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (setIsOpen) {
+      setIsOpen(false);
+    }
+  }, [pathname]);
+
+  return {
+    isOpen,
+    openToggle,
+    closeToggle,
+  };
+}
