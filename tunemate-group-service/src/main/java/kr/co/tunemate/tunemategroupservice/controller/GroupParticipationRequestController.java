4b522a504631ae53012f9a8ef6ce50213b3261ae
고범수 <rhqjatn2398@naver.com>
2023-11-10 14:09:09 +0900

    Feat: 공고참여요청 수락 컨트롤러 메서드 구현

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/controller/GroupParticipationRequestController.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/controller/GroupParticipationRequestController.java
@@ -1,5 +1,6 @@
 package kr.co.tunemate.tunemategroupservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import kr.co.tunemate.tunemategroupservice.service.GroupParticipationRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -7,16 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/groups")
 @RequiredArgsConstructor
 public class GroupParticipationRequestController {
     private final GroupParticipationRequestService groupParticipationRequestService;
 
-    @PostMapping("/{groupId}/participation-requests")
+    @PostMapping("/groups/{groupId}/participation-requests")
     public ResponseEntity postParticipationRequest(@RequestHeader("UserId") String userId, @PathVariable String groupId) {
         groupParticipationRequestService.saveGroupParticipationRequest(userId, groupId);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(description = "공고에 대한 참여요청을 작성자가 수락합니다.")
+    @PostMapping("/group-participation-requests/{groupParticipationRequestId}")
+    public ResponseEntity acceptGroupParticipationRequest(@RequestHeader("UserId") String userId, @PathVariable String groupParticipationRequestId) {
+        groupParticipationRequestService.acceptGroupParticipationRequest(userId, groupParticipationRequestId);
+
+        return ResponseEntity.ok().build();
+    }
 }
