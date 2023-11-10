622df8bcb7030127591cd5239abb2a8f2c891721
고범수 <rhqjatn2398@naver.com>
2023-11-10 09:55:55 +0900

    Fix: 오타 수정

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupParticipationRequestRepository.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupParticipationRequestRepository.java
@@ -1,6 +1,7 @@
 package kr.co.tunemate.tunemategroupservice.repository;
 
+import kr.co.tunemate.tunemategroupservice.entity.GroupParticipationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GroupParticipationRequestRepository extends JpaRepository<GroupParticipationRequestRepository, Long> {
+public interface GroupParticipationRequestRepository extends JpaRepository<GroupParticipationRequest, Long> {
 }
