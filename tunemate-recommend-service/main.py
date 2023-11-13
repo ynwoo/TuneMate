5c0e3d2ad2c22cbd14b39405b9cbdb6edd216f50
남완희 <xjffla12@naver.com>
2023-11-13 09:48:25 +0900

    Fix: None 데이터 처리

--- tunemate-recommend-service/main.py
+++ tunemate-recommend-service/main.py
@@ -249,7 +249,11 @@ def root(UserId: str | None = Header(default=None)):
         cursor.execute(sql, recommend[i])
         playlistId = cursor.fetchall()[0][0]
         userOb = request(recommend[i])
-        responseList.append(ReturnDto(userId=userOb.get("userId"), img=userOb.get("imageUrl"), name=userOb.get("name"),
+        if userOb.get("userId") is None || userOb.get("name") is None:
+            responseList.append(ReturnDto(userId="dummy", img="dummy", name="dummy",
                                       playlist=playlistId, similarity=similaritys[i]))
+        else:
+            responseList.append(ReturnDto(userId=userOb.get("userId"), img=userOb.get("imageUrl"), name=userOb.get("name"),
+                                        playlist=playlistId, similarity=similaritys[i]))
 
     return responseList
