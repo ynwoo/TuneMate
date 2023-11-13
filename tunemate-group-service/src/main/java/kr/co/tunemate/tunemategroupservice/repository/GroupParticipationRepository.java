b12d9e678c9b71a5ba629ae21e47f560c406cc52
고범수 <rhqjatn2398@naver.com>
2023-11-13 12:28:44 +0900

    Feat: 사용자 UUID로 공고참여 목록 조회 추상 메서드 작성

--- tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupParticipationRepository.java
+++ tunemate-group-service/src/main/java/kr/co/tunemate/tunemategroupservice/repository/GroupParticipationRepository.java
@@ -4,8 +4,10 @@ import kr.co.tunemate.tunemategroupservice.entity.Group;
 import kr.co.tunemate.tunemategroupservice.entity.GroupParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupParticipationRepository extends JpaRepository<GroupParticipation, Long> {
     Optional<GroupParticipation> findByUserIdAndGroup(String userId, Group group);
+    List<GroupParticipation> findAllByUserId(String userId);
 }
