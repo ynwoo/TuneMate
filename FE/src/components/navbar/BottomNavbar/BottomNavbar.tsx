7e14ff54d6c99649652b33c842d44b08d3f9c6a1
민동후 <mdh9701@naver.com>
2023-11-14 17:38:57 +0900

    Feat: 자기 프로필 페이지로 연결하는 코드 추가

--- FE/src/components/navbar/BottomNavbar/BottomNavbar.tsx
+++ FE/src/components/navbar/BottomNavbar/BottomNavbar.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
+import React from "react";
 import styles from "./BottomNavbar.module.css";
 import Icon from "@/components/icons";
+import { Cookie } from "@/utils/cookie";
 
 const BottomNavbar = () => {
+  const [userId, setUserId] = React.useState("");
+
+  React.useEffect(() => {
+    if (Cookie.getUserId() !== null) {
+      setUserId(Cookie.getUserId());
+    }
+  }, []);
+
   return (
     <nav className={styles["bottom-navbar"]}>
       <Link href="/main" className={styles["bottom-navbar__item"]}>
         <Icon.Home />
       </Link>
-      <Link href="/profile/1" className={styles["bottom-navbar__item"]}>
+      <Link
+        href={`/profile/${userId}`}
+        className={styles["bottom-navbar__item"]}
+      >
         <Icon.Profile />
       </Link>
       <Link href="/friends" className={styles["bottom-navbar__item"]}>
