c59be9e18021a5b759a8a88cfc85dde2b67b099c
xjffla12 <xjffla12@naver.com>
2023-11-17 14:03:16 +0900

    Refactor: dummy 데이터 이름 변경

--- tunemate-recommend-service/main.py
+++ tunemate-recommend-service/main.py
@@ -194,6 +194,7 @@ def song(UserId: str | None = Header(default=None)):
 @app.get("/recommendation/friends", response_model=List)
 def root(UserId: str | None = Header(default=None)):
     randomName = ["고범희","고범우","남완수","남완우","김윤수","김윤희","고윤우","김범수","남윤우","남범수","강한나","강한수","권현우","권현수","박성우","박성수","박성희","박현우","권성준","양윤우","양우철","권기윤","박은수","박은우","김민아","김민수","김민우","김민희","신민철","곽민철","한동민","권나라","이승기","강호동","유재석","송민호","장원영","하동훈","탁재훈","이상민","김준호","김정은","김정일","공효진","장나라","이정재","장우성","정지훈","김재헌","박지성","손흥민","황희찬","이강인","박주영","이청용","기성용","지동원","설기현","서장훈","민경훈","옥택연","김병만","박정희","김근우","이창원","송주형","류근필","이태준","고은영","김도운","이재용","서수정","우찬희","박은정","양승현","백건우","김진현","박주원","박준원","서주원"]
+    nameSet = {1: "고범순", 2: "김도식", 3: "김민석", 4: "이찬영", 5: "김운수", 6: "김근우", 7: "강한나", 8: "권연우", 9: "박상준", 10: "민동우", 12345: "남상희",1234:"채겨울",1111:"노선균"}
     conn = pymysql.connect(user=os.environ["DATABASE_USERNAME"],
                            password=os.environ["DATABASE_PASSWORD"], host=os.environ["DATABASE_URL"],
                            db="MUSIC", port=int(os.environ["DATABASE_PORT"]), charset="utf8")
@@ -265,7 +266,7 @@ def root(UserId: str | None = Header(default=None)):
         playlistId = cursor.fetchall()[0][0]
         userOb = request(recommend[i])
         if userOb is None or userOb.get("userId") is None or userOb.get("name") is None:
-            responseList.append(ReturnDto(userId=recommend[i], img="https://i1.ruliweb.com/img/22/01/09/17e3e74fd63505a96.jpg", name=random.choice(randomName),
+            responseList.append(ReturnDto(userId=recommend[i], img="https://i1.ruliweb.com/img/22/01/09/17e3e74fd63505a96.jpg", name=nameSet.get(recommend[i]),
                                       playlist=playlistId, similarity=similaritys[i]))
         else:
             responseList.append(ReturnDto(userId=userOb.get("userId"), img=userOb.get("imageUrl"), name=userOb.get("name"),
