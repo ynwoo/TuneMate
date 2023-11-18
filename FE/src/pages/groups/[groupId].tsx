b0559d98dac2798bfcd444b5bda3c78c6a2026c4
노태균 <taegyunn123@gmail.com>
2023-11-18 17:21:36 +0900

    style: group 버튼 위치 조정

--- FE/src/pages/groups/[groupId].tsx
+++ FE/src/pages/groups/[groupId].tsx
@@ -7,17 +7,17 @@ import { useParams } from "next/navigation";
 import styles from "@/styles/GroupPage.module.css";
 import ButtonWithModal from "@/components/button/ButtonWithModal";
 import useParticipateGroupMutation from "@/hooks/mutations/group/useParticipateGroupMutation";
-import { useCallback, useMemo, useState, useEffect } from "react";
+import { useCallback, useMemo } from "react";
 import useMyGroupIdsQuery from "@/hooks/queries/group/useMyGroupIdsQuery";
 import useSentParticipationGroupIdsQuery from "@/hooks/queries/group/useSentParticipationGroupIdsQuery";
 import Button from "@/components/button/Button";
-import { Storage } from "@/utils/storage";
 import useDeleteGroupMutation from "@/hooks/mutations/group/useDeleteGroupMutation";
+import useUserInfo from "@/hooks/useUserInfo";
 
 const GroupDetail = () => {
   const params = useParams();
-  const groupId = (params?.groupId as string) ?? "0";
-  const [userId, setUserId] = useState<string>("");
+  const groupId = (params?.groupId as string) ?? "";
+  const userInfo = useUserInfo();
   const { data: group } = useGroupQuery(groupId);
   const { data: concert } = useConcertDetailQuery(group?.concertId ?? 0);
   const { mutate: deleteGroup } = useDeleteGroupMutation();
@@ -26,12 +26,12 @@ const GroupDetail = () => {
   const { data: mySentGroupIds } = useSentParticipationGroupIdsQuery();
 
   const isParticipated = useMemo(() => {
-    if (!myGroupIds) return false;
+    if (!myGroupIds || !group) return false;
     return myGroupIds.includes(groupId);
   }, [myGroupIds, groupId]);
 
   const isSent = useMemo(() => {
-    if (!mySentGroupIds) return false;
+    if (!mySentGroupIds || !group) return false;
     return mySentGroupIds.includes(groupId);
   }, [mySentGroupIds, groupId]);
 
@@ -45,37 +45,13 @@ const GroupDetail = () => {
     }
   }, [deleteGroup, groupId]);
 
-  useEffect(() => {
-    setUserId(Storage.getUserId());
-  }, [setUserId]);
-
   return (
     <div className={styles.body}>
       <div className={styles["group-detail-page"]}>
         {group && (
           <>
-            <h1 className={styles["group-detail-page__title"]}>
-              {group.title}
-            </h1>
-            {userId === group.hostId && (
-              <div className={styles["group-detail-page__button-container"]}>
-                <Button
-                  className={styles["group-detail-page__button"]}
-                  color="blue"
-                  onClick={onDelete}
-                >
-                  수정
-                </Button>
-                <ButtonWithModal
-                  className={styles["group-detail-page__button"]}
-                  color="red"
-                  modalMessage="공고를 삭제하시겠습니까?"
-                  onClick={onDelete}
-                >
-                  삭제
-                </ButtonWithModal>
-              </div>
-            )}
+            <h1 className={styles["group-detail-page__title"]}>{group.title}</h1>
+
             {concert && <ConcertItem item={concert} />}
             <div className={styles["group-detail-page__description"]}>
               <ConcertInfoItem
@@ -99,27 +75,55 @@ const GroupDetail = () => {
               <ConcertInfoItem
                 className={styles["group-detail-page__description-item"]}
                 title="모집날짜"
-                description={Time.period(
-                  group.startDateTime,
-                  group.deadline as string
-                )}
+                description={Time.period(group.startDateTime, group.deadline as string)}
               />
               {/* <hr /> */}
             </div>
+            <div className={styles["group-detail-page__button-container"]}>
+              {userInfo?.userId === group?.hostId ? (
+                <>
+                  <Button
+                    className={styles["group-detail-page__button"]}
+                    color="blue"
+                    onClick={onDelete}
+                  >
+                    수정
+                  </Button>
+                  <ButtonWithModal
+                    className={styles["group-detail-page__button"]}
+                    color="red"
+                    modalMessage="공고를 삭제하시겠습니까?"
+                    onClick={onDelete}
+                  >
+                    삭제
+                  </ButtonWithModal>
+                </>
+              ) : (
+                <>
+                  {isParticipated && (
+                    <Button color="red" onClick={() => {}}>
+                      가입 완료
+                    </Button>
+                  )}
 
-            {isParticipated || isSent ? (
-              <Button color="red" onClick={() => {}}>
-                {isParticipated ? "가입 완료" : "신청 완료"}
-              </Button>
-            ) : (
-              <ButtonWithModal
-                color="blue"
-                modalMessage="참가 신청 하시겠습니까?"
-                onClick={onParticipate}
-              >
-                신청
-              </ButtonWithModal>
-            )}
+                  {isSent && (
+                    <Button color="red" onClick={() => {}}>
+                      신청 완료
+                    </Button>
+                  )}
+
+                  {!isParticipated && !isSent && (
+                    <ButtonWithModal
+                      color="blue"
+                      modalMessage="참가 신청 하시겠습니까?"
+                      onClick={onParticipate}
+                    >
+                      신청
+                    </ButtonWithModal>
+                  )}
+                </>
+              )}
+            </div>
           </>
         )}
       </div>
